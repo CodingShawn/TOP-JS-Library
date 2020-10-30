ba28a50a87893f74ce8f6ae99aa3634767717a97
Shawn <codingshawn@gmail.com>
2020-10-30 22:24:53 +0800

    Edited JS logic such that previously shown books will not be displayed repeatedly

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ newBookBtn.addEventListener("click", addBookToLibrary);
 showBooksBtn.addEventListener("click", showBooks);
 
 let myLibrary = [];
+let displayedBooks = [];
 
 function Book(bookTitle) {
     this.title = bookTitle;
@@ -20,12 +21,16 @@ function addBookToLibrary() {
 
 function showBooks() {
     myLibrary.forEach(book => {
-        let div = document.createElement("div");
-        div.className += "book";
-        let p = document.createElement("p");
-        let title = document.createTextNode(book.title);
-        p.appendChild(title);
-        div.appendChild(title);
-        bookDisplay.appendChild(div);
+        if (!displayedBooks.includes(book)) {
+            let div = document.createElement("div");
+            div.className += "book";
+            let p = document.createElement("p");
+            let title = document.createTextNode(book.title);
+            p.appendChild(title);
+            div.appendChild(title);
+            div.classList.add("book")
+            bookDisplay.appendChild(div);
+            displayedBooks.push(book);
+        }
     })
 }
